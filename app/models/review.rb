250a0cb295eef6894acfb0c7f642c62cc069e7f4
App Academy Student <email@email.com>
2020-06-27 16:31:12 -0400

    fixing syntax prob

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   validates :rating, inclusion: { in: (1..5) }
 
   belongs_to :musical
-  foreign_key: :musical_id,
+  foreign_key: :show_id,
   class_name: 'Musical'
 end
